3a1a613fcbc26dc0b6488dcf0b38882e01a8cd0b
Pramod <single.core.developers@gmail.com>
2023-06-07 20:48:30 +0530

    new2

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -27,7 +27,7 @@ const [videos,setVideos] = useState([]);
             setSelectedCategory={setSelectedCategory}
           />
           <Typography className="copyright" variant="body2" sx={{mt: 1.5 , color:'#fff'}}>
-                Copyright 2023 Pramod Pandey
+                Copyright 2023 Pramod Joshi
           </Typography>
       </Box>
         <Box p={2} sx={{overflowY:'auto',height:'90vh',flex:2}}>
